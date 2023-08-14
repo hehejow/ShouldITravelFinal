ce05bf31a9252ec5e1d8f54549d300c61aea3f7c
hehejow <sebastianpape1@gmail.com>
2023-08-14 17:13:45 +0200

    stillworks

---
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -0,0 +1,28 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    // Get the URL entered by the user
+    let formText = document.getElementById('name').value;
+    // Make a fetch request to your server-side API
+    fetch(`/api/analyze?url=${encodeURIComponent(formText)}`)
+        .then(res => res.json())
+        .then(function (response) {
+            console.log(response);
+            // Concatenate the texts from sentence_list
+            let sentenceText = '';
+            for (let i = 0; i < response.sentence_list.length; i++) {
+                sentenceText += response.sentence_list[i].text + '<br>';
+            }
+            // Display the API response in the results element
+            document.getElementById('results').innerHTML = `
+          Agreement: ${response.agreement}<br>
+          Subjectivity: ${response.subjectivity}<br> 
+          Text: ${sentenceText}
+        `;
+        })
+        .catch(function (error) {
+            console.error(error);
+            document.getElementById('results').innerHTML = 'Error occurred while fetching data.';
+        });
+}
+
+export { handleSubmit }
