7e1652ae40258f26d27ed7f0024728898adc9bbe
hehejow <sebastianpape1@gmail.com>
2023-08-14 03:06:08 +0200

    test

--- evaluate-news-nlp/src/client/js/formHandler.js
+++ evaluate-news-nlp/src/client/js/formHandler.js
@@ -8,7 +8,7 @@ function handleSubmit(event) {
     fetch(`/api/analyze?url=${encodeURIComponent(formText)}`)
         .then(res => res.json())
         .then(function(response) {
-            console.log(response)
+            console.log(response);
             // Display the API response in the results element
             document.getElementById('results').innerHTML = `
                 Polarity: ${response.polarity}<br>
@@ -21,4 +21,4 @@ function handleSubmit(event) {
             document.getElementById('results').innerHTML = 'Error occurred while fetching data.';
         });
 }
-export { handleSubmit };
+export { handleSubmit }
