444fa257188632ff10ced92e9e9e3af7c6e8a9e0
hehejow <sebastianpape1@gmail.com>
2023-08-15 03:14:03 +0200

    working

--- evaluate-news-nlp/src/client/index.js
+++ evaluate-news-nlp/src/client/index.js
@@ -2,63 +2,44 @@ import './styles/resets.scss'
 import './styles/base.scss'
 // import { handleSubmit } from './js/formHandler';
 
-// Personal API Key for OpenWeatherMap API
-const apiKey = '12dac6a09e9313bf16c99ca556b22b70&units=imperial';
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+"use strict";
+document.querySelector("#generate").addEventListener("click", function () {
+  const zipCode = document.querySelector("#zip").value;
+  const tripDateInput = document.getElementById("trip-date"); // Declare and initialize the tripDateInput variable
+  const tripDate = new Date(tripDateInput.value); // Access the value of tripDateInput and assign it to tripDate
 
-// Event listener to add function to existing HTML DOM element
-document.querySelector('#generate').addEventListener('click', function () {
-    const zipCode = document.querySelector('#zip').value;
+  // Calculate the remaining days
+  const currentDate = new Date();
+  const timeDifference = tripDate.getTime() - currentDate.getTime();
+  const remainingDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
 
-    // Function called by event listener
-    console.log(zipCode)
-    fetchWeatherData(baseURL, zipCode, apiKey);
+  // Display the remaining days to the user
+  console.log('Remaining days:', remainingDays);
+
+  fetch("/weather", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      zipCode: zipCode,
+      tripDate: tripDate,
+      remainingDays: remainingDays
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Call function to update UI with data from server
+      updateUI(data);
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
 });
 
-// Function to GET Web API Data and POST to server
-async function fetchWeatherData(baseURL, zipCode, apiKey) {
-
-    // String concatination for API endpoint
-    const url = `${baseURL}?zip=${zipCode}&appid=${apiKey}`;
-
-    // Fetching the data...
-    const response = await fetch(url);
-
-    // ...awaiting response
-    const data = await response.json();
-
-    // Get user feelings imput
-    const userResponse = document.querySelector('#feelings').value;
-
-    // Initialize object and reference response data
-    const postData = {
-        temperature: data.main.temp,
-        date: new Date().toLocaleDateString(),
-        userResponse: userResponse
-    };
-
-    // POST to server
-    await fetch('/projectData', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(postData)
-    });
-
-    console.log('WeatherMood posted to server:', postData);
-    // Call function to update UI
-    updateUI();
-}
-// Declare async function to update UI
-async function updateUI() {
-    const request = await fetch('/projectData');
-    try {
-        const allData = await request.json();
-        document.querySelector('#temp').innerHTML = 'Temperature: ' + Math.round(allData.temperature) + ' °F'
-        document.querySelector('#date').innerHTML = 'Date: ' + allData.date;
-        document.querySelector('#content').innerHTML = 'Mood: ' + allData.userResponse;
-    } catch (error) {
-        console.log('error', error);
-    }
+// Function to update UI
+function updateUI(data) {
+  document.querySelector('#temp').innerHTML = 'Temperature: ' + Math.round(data.temperature) + ' °F';
+  document.querySelector('#date').innerHTML = 'Date: ' + data.date;
+  document.querySelector('#content').innerHTML = 'Mood: ' + data.userResponse;
 }
--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -1,56 +1,69 @@
 const dotenv = require('dotenv');
 dotenv.config();
-// Setup empty JS object to act as endpoint for all routes
-let projectData = {};
-
-// Express to run server and routes
 const express = require('express');
-
-// Start up an instance of app
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
-// Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('dist'));
+let projectData = {};
 
-// Cors for cross origin allowance
-const cors = require("cors");
-app.use(cors()); 
-
-// Initialize the main project folder
-app.use(express.static('dist'))
-
-console.log(__dirname)
-
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
-
-// Setup Server
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html');
+});
 const port = 5000;
 const server = app.listen(port, listening);
 
 function listening() {
-    console.log("server running");
-    console.log(`running on localhost: ${port}`);
-};
+    console.log('Server running');
+    console.log(`Running on localhost: ${port}`);
+}
 
-// GET route that returns the projectData object
-app.get('/projectData', function (req, res) {
+// Server-side endpoint to handle API request
+app.post('/weather', (req, res) => {
+  const city = req.body.zipCode;
+  const tripDate = req.body.tripDate; // Access the tripDate value from the request body
+  const remainingDays = req.body.remainingDays; // Access the remainingDays value from the request body
+  const username = 'hehejow';
+  const baseURL = 'http://api.geonames.org/searchJSON';
+
+  // String concatenation for API endpoint
+  const url = `${baseURL}?q=${city}&username=${username}`;
+
+  // Fetching the data from Geonames API
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          // Extracting the location data from the response
+          const locationData = {
+              city: data.geonames[0].name,
+              country: data.geonames[0].countryName,
+              latitude: data.geonames[0].lat,
+              longitude: data.geonames[0].lng
+          };
+
+          // Add the location data to the projectData object
+          projectData = locationData;
+          projectData.tripDate = tripDate; // Add tripDate to projectData
+          projectData.remainingDays = remainingDays
+
+          // Send a response back to the client
+          res.status(200).json(locationData);
+          console.log('Location data sent to client:', locationData);
+      })
+      .catch(error => {
+          console.log('Error:', error);
+          res.status(500).json({
+              error: 'Failed to fetch location data'
+          });
+      });
+});
+
+app.get('/projectData', function(req, res) {
     res.send(projectData);
-})
-
-// POST route
-app.post('/projectData', (req, res) => {
-    const { temperature, date, userResponse } = req.body;
-
-    // Add the data to the projectData object
-    projectData.temperature = temperature;
-    projectData.date = date;
-    projectData.userResponse = userResponse;
-
-    // Send a response back to the client
-    res.status(200).send({ success: true, response: { message: 'Receiving data...' } });
-    console.log('Received data:', req.body);
 });
